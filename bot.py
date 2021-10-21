680df2549e092b1992216ba8a012e802c7538be5
Kittu <70826024+noob-kittu@users.noreply.github.com>
2021-10-22 03:46:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ api_id = int(os.environ.get("APP_ID"))
 api_hash = os.environ.get("API_HASH")
 bot_token = os.environ.get("TOKEN")
 tsf = TelegramClient('client', api_id, api_hash)
-start = tsf.start(bot_token=bot_token)
+RUn = tsf.start(bot_token=bot_token)
 @tsf.on(events.NewMessage(pattern="^/start$"))
 async def start(event):
   await event.reply("__**Tag All Bot**, I can Tag almost all members in group or channel 😉\nClick **/help** for more information__\n\n And Join [LEAGUE OF BOTS]()",
@@ -106,4 +106,4 @@ async def mentionall(event):
         
 
 print ("Successfully Started")
-start.run_until_disconnected()
+RUn.run_until_disconnected()
