c06a119b07e22c1d1c53a4aa4ad9737043e2e0e3
Kittu <70826024+noob-kittu@users.noreply.github.com>
2021-10-22 03:41:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,48 +20,90 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.'''
 
-
-from telethon import TelegramClient
-from telethon import events
-import os
-import logging
+import os, logging, asyncio
+from telethon import Button
+from telethon import TelegramClient, events
 from telethon.tl.types import ChannelParticipantsAdmins
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(name)s - [%(levelname)s] - %(message)s'
+)
+LOGGER = logging.getLogger(__name__)
 
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.WARNING)
+api_id = int(os.environ.get("APP_ID"))
+api_hash = os.environ.get("API_HASH")
+bot_token = os.environ.get("TOKEN")
+tsf = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 
-# Basics
-APP_ID = os.environ.get("APP_ID", default=None)
-API_HASH = os.environ.get("API_HASH", default=None)
-BOT_TOKEN = os.environ.get("BOT_TOKEN", default=None)
-
-
-bot = TelegramClient("MentionBot", APP_ID, API_HASH)
-start = bot.start(bot_token=BOT_TOKEN) 
-
-
-
-@bot.on(events.NewMessage(pattern="^/start"))
-async def _(event):
-    await event.reply('''Hey, I'm Mention Bot. I'll tag your group members and will notify them.\n\nMade by @Autichrist''')
-@bot.on(events.NewMessage(pattern="^/mention ?(.*)"))
-async def _(event):
-    if event.fwd_from:
-        return
-    mentions = "Users : "
-    chat = await event.get_input_chat()
-    async for x in bot.iter_participants(chat, filter=ChannelParticipantsAdmins):
-        mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
-    reply_message = None
-    if event.reply_to_msg_id:
-        reply_message = await event.get_reply_message()
-        await reply_message.reply(mentions)
-    else:
-        await event.reply(mentions)
-    await event.delete()
-
-
+@tsf.on(events.NewMessage(pattern="^/start$"))
+async def start(event):
+  await event.reply("__**Tag All Bot**, I can Tag almost all members in group or channel 😉\nClick **/help** for more information__\n\n And Join [T.S.F]()",
+                    buttons=(
+                      [Button.url('📶Network', 'https://t.me/league_of_bots'),
+                      Button.url('🚀 Github', 'https://github.com/Noob-kittu')]
+                    ),
+                    link_preview=False
+                   )
+@tsf.on(events.NewMessage(pattern="^/help$"))
+async def help(event):
+  helptext = "**Help Menu of TagAll**\n\nCommand: /tagall\n__You can use this command with text what you want to Tag others.__\n`Example: /tagall Good Morning!`\n__You can you this command as a reply to any message. Bot will tag users to that replied messsage__."
+  await event.reply(helptext,
+                    buttons=(
+                      [Button.url('📶Network', 'https://t.me/league_of_bots'),
+                      Button.url('🚀 Github', 'https://github.com/Noob-kittu')]
+                    ),
+                    link_preview=False
+                   )
+  
+@tsf.on(events.NewMessage(pattern="^/tagall ?(.*)"))
+async def mentionall(event):
+  if event.is_private:
+    return await event.respond("__This command can be use in groups and channels!__")
+  
+  admins = []
+  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
+    admins.append(admin.id)
+  if not event.sender_id in admins:
+    return await event.respond("__Only admins can mention all!__")
+  
+  if event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.reply_to_msg_id:
+    mode = "text_on_reply"
+    msg = event.reply_to_msg_id
+    if msg == None:
+        return await event.respond("__I can't mention members for older messages! (messages which are sent before I'm added to group)__")
+  elif event.pattern_match.group(1) and event.reply_to_msg_id:
+    return await event.respond("__Give me one argument!__")
+  else:
+    return await event.respond("__Reply to a message or give me some text to mention others!__")
+  
+  if mode == "text_on_cmd":
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+      if usrnum == 5:
+        await client.send_message(event.chat_id, f"{usrtxt}\n\n{msg}")
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+        
+  if mode == "text_on_reply":
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+      if usrnum == 5:
+        await client.send_message(event.chat_id, usrtxt, reply_to=msg)
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+        
 
 print ("Successfully Started")
 start.run_until_disconnected()
