0b30e8cc13b46889a9ed68f433daecef5b8665a1
Kittu <70826024+noob-kittu@users.noreply.github.com>
2021-10-22 03:47:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,7 +33,7 @@ LOGGER = logging.getLogger(__name__)
 
 api_id = int(os.environ.get("APP_ID"))
 api_hash = os.environ.get("API_HASH")
-bot_token = os.environ.get("TOKEN")
+bot_token = os.environ.get("BOT_TOKEN")
 tsf = TelegramClient('client', api_id, api_hash)
 RUn = tsf.start(bot_token=bot_token)
 @tsf.on(events.NewMessage(pattern="^/start$"))
